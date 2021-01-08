7f1e078c74c950b8f3fcf8b8c8abf0ff0c1a474a
İlknur SUCAKLI <isucakli@aof.anadolu.edu.tr>
2021-01-09 00:30:30 +0300

    odev2

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+	render() {
+		return (
+			<footer className="footer">
+				<span className="todo-count">
+					<strong>{this.props.todoLength} </strong>
+					items left
+				</span>
+
+				<ul className="filters">
+					<li>
+						<a onClick={this.props.filterAll} className="selected">All</a>
+					</li>
+					<li>
+						<a onClick={this.props.filterActive}>Active</a>
+					</li>
+					<li>
+						<a onClick={this.props.filterCompleted}>Completed</a>
+					</li>
+				</ul>
+
+				<button className="clear-completed" onClick={this.props.clearItems}>Clear completed</button>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
